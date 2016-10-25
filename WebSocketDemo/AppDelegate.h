4920e6f6f55a7b6a6788065b9e3beafe32d93136
948769029 <apple@appledeMacBook-Pro.local>
2016-10-25 15:59:14 +0800

    Initial Commit

---
+++ WebSocketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebSocketDemo
+//
+//  Created by apple on 2016/10/25.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
