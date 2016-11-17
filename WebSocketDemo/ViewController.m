a93184d91aad33b87a0e0420f649d3728819a7ec
948769029 <apple@appledeMacBook-Pro.local>
2016-11-17 16:14:12 +0800

    更改URL

--- WebSocketDemo/ViewController.m
+++ WebSocketDemo/ViewController.m
@@ -49,74 +49,20 @@
     self.webSocket.delegate = self;
     [self.webSocket close];
     
-    // 172.16.2.173
-    // 172.16.2.246
-    // 172.16.2.1
-    // ws://172.16.6.142:8090/dzfchat
-    
-//    ws://172.16.6.142:8090/dzfchat/rqQpVqnbmh/qpsTcFjOO94GKMxGJ8vChkmH+HFmaH7w=/chatservlet.ws
-    
-    self.webSocket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ws://172.16.6.142:8090/dzfchat/bca7fb5d1df27929163ea63a4de71dde74d91e1290571dbee34e3818e4daea90/chatservlet.ws"]]];
+    self.webSocket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"自己服务器聊天地址"]]];
     self.webSocket.delegate = self;
     self.title = @"Opening Connecting";
     NSLog(@"------222------");
     [self.webSocket open];
-    
-//    self.manager = [MessageManager sharedInstance];
-//    self.manager.socketHost = @"ws://172.16.6.142:8090/dzfchat/bca7fb5d1df27929163ea63a4de71dde74d91e1290571dbee34e3818e4daea90/chatservlet.ws";
-//    [self.manager socketConnect];
 }
 
 #pragma mark - SendButton Response
 - (void)sendAction:(id)sender {
     
-    
-//username: mine.username
-//    ,avatar:mine.avatar
-//    ,id: mine.id
-//    ,type:  to.type
-//    ,content:mine.content
-//    ,message_type: 'chatMessage'
-//    ,to_id:to.id
-//    ,to_username:to.username
-//    ,to_avatar:to.avatar
-    
-    NSDictionary *dict = @{@"username" : @"123",
-                           @"avatar":@"",
-                           @"id":@"appuse00000000aAosRX0001",
-                           @"type":@"friend",
-                           @"content":@"nihao1234567890",
-                           @"message_type":@"chatMessage",
-                           @"to_id":@"00000100000000d2fSVy0002",
-                           @"to_username":@"456",
-                           @"to_avatar":@"",};
-    
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:dict];
-    
-    [self.view endEditing:YES];
-    
-    NSLog(@"%@",[self DataTOjsonString:dict]);
-    
     // WebSocket
-//    if (self.webSocket) {
-//        [self.webSocket send:[self DataTOjsonString:dict]];
-//    }
-    [self.manager.socket send:[self DataTOjsonString:dict]];
-}
-
--(NSString*)DataTOjsonString:(id)object
-{
-    NSString *jsonString = nil;
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:object
-                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
-                                                         error:&error];
-    if (! jsonData) {
-        NSLog(@"Got an error: %@", error);
-    } else {
-        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    if (self.webSocket) {
+        [self.webSocket send:@"您好~"];
     }
-    return jsonString;
 }
 
 //连接成功
